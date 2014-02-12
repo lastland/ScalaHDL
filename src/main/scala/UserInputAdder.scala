bd930190db52b83e56a38c38e8dcc2f79fb89535
Yao Li <hnkfliyao@gmail.com>
2014-02-12 18:38:48 +0800

    An example of getting numbers from user inputs.

---
+++ src/main/scala/UserInputAdder.scala
@@ -0,0 +1,46 @@
+package ScalaHDLExample.UserInput.Add
+
+import ScalaHDL.Core.ScalaHDL
+import ScalaHDL.Core.HDLType
+import ScalaHDL.Core.DataType._
+import ScalaHDL.Core.DataType.Signals._
+import ScalaHDL.Simulation.Simulator
+
+object Main extends ScalaHDL {
+  defMod.adder('clk, 'rst, 'a, 'b, 'z) {
+    sync(1).add {
+      when ('rst is 1) {
+        'z := 0
+      } .otherwise {
+        'z := 'a + 'b
+      }
+    }
+
+    defMod.Bench('clk, 'rst, 'a, 'b, 'z) {
+      delay(1) {
+        cycle('clk)
+      }
+
+      sync(0) {
+        'rst := 0
+        'a := Console.readInt()
+        'b := Console.readInt()
+      }
+    }
+  }
+
+  def main(args: Array[String]) {
+    val clk = bool(0)
+    val rst = bool(0)
+    val a = signed(0, 5)
+    val b = signed(0, 5)
+    val z = signed(0, 6)
+
+    val sim = Simulator(Main,
+      module('adder, clk, rst, a, b, z),
+      module('Bench, clk, rst, a, b, z))
+    sim.setTrace("add-ui.vcd")
+    sim.simulate(10)
+    sim.stop()
+  }
+}
